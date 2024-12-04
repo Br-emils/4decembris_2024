c5baebe7943cbb3d27a0a6a2bfc582de538b3452
Br-emils <emils.brikmanis@grobinasskola.lv>
2024-12-04 14:31:59 +0200

    komentiV2

--- 03/main.py
+++ 03/main.py
@@ -1,4 +1,4 @@
-#dotie skaitļi
+#Masīvs
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
 num = int(input("Choose a number: "))
@@ -8,6 +8,6 @@ new_list = []
 for i in a:
     if i < num:
         new_list.append(i)
-#izveido sarakstu kas parādīs visus dotos skaitļus līdz tavam skaitlim-1 (ja ievadīts 13 tad termināli 13 nerādīsies)
+#Pievieno sarakstu kas parādīs visus masīva skaitļus līdz tavam skaitlim-1 (ja ievadīts 13 tad termināli 13 nerādīsies)
 print(new_list)
 #parāda to terminālī
