3093c0bc72ee70aaf6b4b5276cd8b8d3319f562b
Br-emils <emils.brikmanis@grobinasskola.lv>
2024-12-04 14:24:29 +0200

    komenti

--- 03/main.py
+++ 03/main.py
@@ -1,11 +1,13 @@
+#dotie skaitļi
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
 num = int(input("Choose a number: "))
-
+#ievadi skaitli
 new_list = []
 
 for i in a:
     if i < num:
         new_list.append(i)
-
+#izveido sarakstu kas parādīs visus dotos skaitļus līdz tavam skaitlim-1 (ja ievadīts 13 tad termināli 13 nerādīsies)
 print(new_list)
+#parāda to terminālī
